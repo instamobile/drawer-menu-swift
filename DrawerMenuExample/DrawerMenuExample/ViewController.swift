2639bb6a89a6ea41e1b64e0cbcc95812089f6c62
khanh.duy29771@gmail.com <khanh.duy29771@gmail.com>
2019-11-01 18:29:59 +0700

    Updated latest version swift 5

--- DrawerMenuExample/DrawerMenuExample/ViewController.swift
+++ DrawerMenuExample/DrawerMenuExample/ViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let mainVC = UIViewController()
-        mainVC.view.backgroundColor = .red
-
-        let rootController = RootViewController(mainViewController: mainVC, topNavigationLeftImage: UIImage(named: "hamburger-menu-icon"))
-        let menuVC = MenuViewController()
-        menuVC.view.backgroundColor = .green
-
-        let drawerVC = DrawerController(rootViewController: rootController, menuController: menuVC)
-
-        self.addChildViewController(drawerVC)
-        view.addSubview(drawerVC.view)
-        drawerVC.didMove(toParentViewController: self)
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let mainVC = UIViewController()
+    mainVC.view.backgroundColor = .red
+    
+    let rootController = RootViewController(mainViewController: mainVC, topNavigationLeftImage: UIImage(named: "hamburger-menu-icon"))
+    let menuVC = MenuViewController()
+    menuVC.view.backgroundColor = .green
+    
+    let drawerVC = DrawerController(rootViewController: rootController, menuController: menuVC)
+    self.addChild(drawerVC)
+    view.addSubview(drawerVC.view)
+    drawerVC.didMove(toParent: self)
+  }
 }
